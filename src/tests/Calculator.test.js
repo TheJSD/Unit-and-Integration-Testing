b7aacc29c9bff5f81b8efbea6458d795ff686d36
James Dickson <jamesstuartdickson@gmail.com>
2023-10-27 10:51:59 +0100

    add 1 to 4 gets 5 unit test written

--- src/tests/Calculator.test.js
+++ src/tests/Calculator.test.js
@@ -14,5 +14,17 @@ describe('Calculator', () => {
     fireEvent.click(button4);
     expect(runningTotal.textContent).toEqual('4');
   })
-})
 
+  it('should return 5 when 1 is added to 4', () => {
+    const button4 = container.getByTestId('number4');
+    const button1 = container.getByTestId('number1');
+    const buttonAdd = container.getByTestId('operator-add');
+    const buttonEquals = container.getByTestId('operator-equals');
+    const runningTotal = container.getByTestId('running-total');
+    fireEvent.click(button4);
+    fireEvent.click(buttonAdd);
+    fireEvent.click(button1);
+    fireEvent.click(buttonEquals);
+    expect(runningTotal.textContent).toEqual('5');
+  })
+})
